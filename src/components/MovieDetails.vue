8fb62fd36427222a74eb621cb2bb262412382e76
hiiamhip <trantuanhiep420@gmail.com>
2025-02-27 09:46:40 +0000

    half

---
+++ src/components/MovieDetails.vue
@@ -0,0 +1,9 @@
+<template>
+	<div v-if="movie" class="z-40 text-white w-[53%] p-20">
+		<div class="text-[55px] font-semibold font-serif">{{ movie.name }}</div>
+	</div>
+</template>
+
+<script setup>
+	defineProps({ movie: Object })
+</script>
